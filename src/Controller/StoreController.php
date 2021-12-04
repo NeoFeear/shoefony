505a9f3ac440dc35c6478caa063dfa4fd42a99fa
Florian Martin <neofeear@gmail.com>
2021-12-04 10:46:44 +0100

    IP by Request

--- src/Controller/StoreController.php
+++ src/Controller/StoreController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,13 +12,13 @@ class StoreController extends AbstractController
     /**
      * @Route("/store/product/{id}/details/{slug}", name="store_show_product", requirements={"id"="\d+"})
      */
-    public function homepage(int $id, string $slug):Response
+    public function homepage(int $id, string $slug, Request $request):Response
     {
         return $this->render('store/product.html.twig', [
             'controller_name' => 'StoreController',
             'id' => $id,
             'slug' => $slug,
-            'ip' => $_SERVER['REMOTE_ADDR']
+            'ip' => $request->getClientIp()
         ]);
     }
 }
